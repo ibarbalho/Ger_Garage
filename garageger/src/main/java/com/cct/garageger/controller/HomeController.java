a6a552bac2eebdb16978a03d410ce8cededcc837
Ittalo <Ittalo@192.168.0.192>
2020-08-15 19:33:00 +0100

    Url Home page

--- garageger/src/main/java/com/cct/garageger/controller/HomeController.java
+++ garageger/src/main/java/com/cct/garageger/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	//Return the page Home
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home() {
 		return "home";
 	}
